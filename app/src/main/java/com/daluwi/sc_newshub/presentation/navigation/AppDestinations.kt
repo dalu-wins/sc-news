1eb885a3b2e05a1b345d005b4e56324b9677bef9
Daniil Wins <dalu-wins@users.noreply.github.com>
2025-10-18 17:50:58 +0200

    removed unused imports

--- app/src/main/java/com/daluwi/sc_newshub/presentation/navigation/AppDestinations.kt
+++ app/src/main/java/com/daluwi/sc_newshub/presentation/navigation/AppDestinations.kt
@@ -1,12 +1,9 @@
 package com.daluwi.sc_newshub.presentation.navigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Album
 import androidx.compose.material.icons.filled.Looks3
 import androidx.compose.material.icons.filled.LooksOne
 import androidx.compose.material.icons.filled.LooksTwo
-import androidx.compose.material.icons.filled.MusicNote
-import androidx.compose.material.icons.filled.PlaylistAddCircle
 import androidx.compose.ui.graphics.vector.ImageVector
 
 enum class AppDestinations(
