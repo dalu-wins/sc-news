74871f83220c1bb52b930e8ee103a7cc7fff5d1c
Daniil Wins <dalu-wins@users.noreply.github.com>
2025-10-24 20:27:54 +0200

    third test-release, signed release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "0.1.1"
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
