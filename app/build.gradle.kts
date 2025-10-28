6fa457f521f3f00afc38d9b1d50457d32102d4e5
Daniil Wins <dalu-wins@users.noreply.github.com>
2025-10-28 17:13:30 +0100

    fix: test new branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         minSdk = 31
         targetSdk = 36
 
-        versionCode = 105
-        versionName = "0.1.5"
+        versionCode = 104
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
