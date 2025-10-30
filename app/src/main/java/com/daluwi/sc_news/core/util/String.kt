00bfaf85c3a0a1981835847d40c7acaa2212f200
Daniil Wins <dalu-wins@users.noreply.github.com>
2025-10-30 21:00:23 +0100

    chore: remove unused function

--- app/src/main/java/com/daluwi/sc_news/core/util/String.kt
+++
@@ -1,7 +0,0 @@
-package com.daluwi.sc_news.core.util
-
-// TODO use in Channel parsing or make api provide reliable channel info
-private fun String.containsAny(keywords: List<String>): Boolean {
-    val upperThis = this.uppercase()
-    return keywords.any { upperThis.contains(it.uppercase()) }
-}
