f219fb91c38ce5d38335ed9d2318a586c2950a76
Daniil Wins <dalu-wins@users.noreply.github.com>
2025-10-30 20:53:02 +0100

    fix: animate loading indicator, fade-in fade-out

--- app/src/main/java/com/daluwi/sc_news/features/patches/presentation/PatchScreen.kt
+++ app/src/main/java/com/daluwi/sc_news/features/patches/presentation/PatchScreen.kt
@@ -1,9 +1,11 @@
 package com.daluwi.sc_news.features.patches.presentation
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -45,8 +47,10 @@ fun PatchScreen(
             )
         ) {
 
-            if (state.isLoading) {
-                item { LoadingIndicator(modifier = Modifier.fillMaxSize()) }
+            item {
+                AnimatedVisibility(state.isLoading, modifier = Modifier.fillMaxWidth()) {
+                    LoadingIndicator(modifier = Modifier.fillMaxSize())
+                }
             }
 
             pinnedSection(
